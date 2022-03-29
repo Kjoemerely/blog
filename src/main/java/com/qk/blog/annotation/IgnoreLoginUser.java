5b05185095dbf4e2c30cfbeac2ca801bac2f6a69
Kjoemerely <kjoemerely@outlook.com>
2022-03-29 10:41:15 +0800

    Update IgnoreLoginUser.java

--- src/main/java/com/qk/blog/annotation/IgnoreLoginUser.java
+++ src/main/java/com/qk/blog/annotation/IgnoreLoginUser.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
  * Title:自定义注解
  * Description: 标识Mapper切面是否查询登录用户信息
  *
- * @author rico
+ * @author merely
  * @created 2017年7月4日 下午4:25:32
  */
 @Target(ElementType.METHOD)
