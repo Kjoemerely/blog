b2007c4bcd610f2cc79a76ea431d790cbec77aec
Kjoemerely <kjoemerely@outlook.com>
2022-03-29 10:41:35 +0800

    Update IgnoreSecurity.java

--- src/main/java/com/qk/blog/annotation/IgnoreSecurity.java
+++ src/main/java/com/qk/blog/annotation/IgnoreSecurity.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
  * Title:自定义注解
  * Description: 标识是否忽略REST安全性检查
  *
- * @author rico
+ * @author merely
  * @since 2017年7月4日 下午4:25:32
  */
 @Target(ElementType.METHOD)
